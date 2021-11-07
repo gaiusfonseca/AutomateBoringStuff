35448e0b0ce4a4d88b6d1997d7e1277643ee89b9
Gaius Fonseca <gaiusfonseca@gmail.com>
2021-11-07 01:51:41 -0300

    exercicios resolvidos capitulo 4

---
+++ Cap04/StringFromList.py
@@ -0,0 +1,26 @@
+""" Say you have a list value like this:
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+Write a function that takes a list value as an argument and returns a string with all the items separated by a comma and
+a space, with and inserted before the last item. For example, passing the previous spam list to the function would 
+return 'apples, bananas, tofu, and cats' . But your function should be able to work with any list value passed to it."""
+
+def listToString(spam):
+    myString = ''
+
+    for i in range(len(spam)):
+        myString += spam[i]
+        
+        if i == len(spam) - 1:
+            myString += ''
+        elif i == len(spam) - 2:
+            myString += ' and '
+        else:
+            myString += ', '
+    
+    return myString
+
+spam = ['Java', 'Javascript', 'Python', 'Kotlin', 'Go']
+
+print(listToString(spam))
